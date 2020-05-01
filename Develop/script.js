41577e692eac5877891f7a80425354c98460744d
Jordan M <jordan.muliaman@gmail.com>
2020-04-30 18:30:24 -0700

    add javascript and update readme

---
+++ Develop/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+  // grab current day id and input time and date
+  $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm a'))
+
+  // make variable for current hour
+  let currentHour = moment().hours()
+  let savedHours = []
+  let savedText = []
+  
+  // do a function for the total hours
+  function totalHours() {
+
+    // make a function for each time block to display color coded past, present or future based on the time
+    $('.time-block').each(function () {
+
+      // since the row values are the same, use attr to grab first 'id' element
+      let hourElem = $(this).attr('id')
+      // find out what 'this' is by console logging
+      console.log(this)
+      // remove 'hour-' from the 'id' by using slice() and it does the same throughout the entire length of the element 
+      let dayHours = hourElem.slice(5, hourElem.length)
+      // use parseInt() to parse string and return it as an integer
+      let dayHoursInt = parseInt(dayHours)
+      let currentHourInt = parseInt(currentHour)
+
+      // if the hour is equal to the current hour remove past and future and show present
+      if (dayHoursInt === currentHourInt) {
+        $(this).removeClass('past')
+        $(this).removeClass('future')
+        $(this).addClass('present')
+        // if hour is less than current hour it means that it has passed so show past and remove present and future
+      } else if (dayHoursInt < currentHourInt) {
+        $(this).removeClass('present')
+        $(this).removeClass('future')
+        $(this).addClass('past')
+        // if hour is more than current hour means that current hour hasn't reached the time yet so show future
+      } else if (dayHoursInt > currentHourInt) {
+        $(this).removeClass('past')
+        $(this).removeClass('present')
+        $(this).addClass('future')
+      }
+
+    })
+
+  }
+  totalHours()
+
+  // make a function to save the hour
+  function savedHour() {
+    // check browser support
+    if (typeof Storage !== 'undefined') {
+      // store saved hours to local storage
+      localStorage.setItem('savedHours', currentHour)
+    }
+  }
+
+  // make a function to save the text
+  function saveText() {
+    let plans = document.querySelector('.description').value
+    // check browser support
+    if (typeof Storage !== 'undefined') {
+      // store saved text to local storage
+      localStorage.setItem('savedText', plans)
+    }
+    savedHour()
+  }
+})
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Day-Planner
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+* This was a pretty challenging task. With lots of trials and errors I was able to use the CSS that was given and use it for the HTML and Javascript.
+* I first start with the HTML page then moved on to Javascript
+* I start inputting the time and date to the jumbotron then I made a function for the total hour from 9-5. Since the values of each row are the same, I had to grab the very first id by using attributes. Since the id was 'hour-#' I had to slice the first 5 words so we're only left with a number string. Since the hours are strings I had to make it an integer by using parseInt(). From there we can start doing if and then statement to get past, present and future. 
+* Storing the input text was challenging and I still wasn't able to get the result I wanted.
